3a65ae03f68e1c269c53853f8e65832ae1bd1483
romiriquelme <romiriquelme230@gmail.com>
2024-07-18 21:39:21 +0800

    Menambahkan Sesuatu

--- homepage/home.php
+++ homepage/home.php
@@ -4,9 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-
+    <style>
+        body{
+            height: 4em;
+            background-color: black;
+        }
+    </style>
 </head>
 <body>
-    
+        <?php
+        echo "Hello World"
+        ?>
+        
 </body>
 </html>
---
+++ homepage/profil.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QuoRun | Profile</title>
+</head>
+
+<style type="text/css">
+#blue_bar {
+    height: 50px;
+    background-color: rgb(59, 89, 152);
+    color: white;
+}
+
+#search_box {
+    width: 400px;
+    height: 20px;
+}
+
+#profile_pic {
+    width: 150px;
+
+}
+</style>
+
+<body style="font-family: tahoma; margin: auto; font-size:30px; background-color: rgb(175, 172, 167);">
+
+
+
+    <!-- blue bar -->
+    <div id="blue_bar">
+        <div style="width: 800px; margin:auto; font-size: 30px;">
+
+            QuoRun &nbsp &nbsp<input type="text" id="search_box">
+            <img src="/FolderFoto/member-9.png" style="width: 50px; float: right;">
+        </div>
+    </div>
+
+
+    <!-- cover area -->
+    <div style="width: 800px; margin: auto; background-color: black; min-height: 400px;">
+
+        <div style="background-color: white; text-align: center; color: #405d9b">
+
+            <img src="/FolderFoto/feed-image-5.png" style="width:100%">
+            <img id="profile_pic" src="/FolderFoto/member-8.png">
+
+            <br> Answers Questions About Settings
+
+        </div>
+    </div>
+
+
+
+</body>
+
+</html>
